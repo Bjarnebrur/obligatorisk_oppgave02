6c43b08cdba63771cd07f816611da6478ea53c49
Carl Fredrik <cadok9842.usn.no>
2025-10-21 22:29:55 +0200

    gfsd

--- public/registrer_student.php
+++ public/registrer_student.php
@@ -1,1 +1,23 @@
+<?php  
+    include ("db.php"); /*kobler opp mot databasen*/
+    if(issit($_POST["submit"])){} /*sjekker om vi har sendt inn ett skjema*/
 
+
+?>
+
+<!DOCTYPE html>
+<html lang="no">
+  <head>
+    <meta charset="UTF-8">
+</head>
+<body>
+  <h3>Register ny student</h3>
+
+  
+    <form method="post" actio=""> <!-- lager et skjema boi-->
+        <label>Brukenavn:</label><br>
+        <input type="text" name="brukernavn"><<br><br>
+    
+  </table>
+</body>
+</html>
