f14def26c78cbea46bdca02cdc6791ae6b331512
Carl Fredrik <cadok9842.usn.no>
2025-10-21 22:36:05 +0200

    gfa

--- public/registrer_student.php
+++ public/registrer_student.php
@@ -16,8 +16,20 @@
   
     <form method="post" actio=""> <!-- lager et skjema boi-->
         <label>Brukenavn:</label><br>
-        <input type="text" name="brukernavn"><<br><br>
-    
+        <input type="text" name="brukernavn"><br><br>
+
+        <label>Brukenavn:</label><br>
+        <input type="text" name="fornavn"><br><br>
+
+        <label>Brukenavn:</label><br>
+        <input type="text" name="etternavn"><br><br>
+
+        <label>Brukenavn:</label><br>
+        <input type="text" name="klassekode"><br><br>
+
+        <input type="submit" name="submit" value="Registrer student">
+    </form>
+    <p><a href="index.php">Tilbake til hovedmeny</a></p>
   </table>
 </body>
 </html>
