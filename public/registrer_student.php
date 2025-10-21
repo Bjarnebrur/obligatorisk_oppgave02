11d8dc95a59bc70500fa40ba0db343788b656d50
Carl Fredrik <cadok9842.usn.no>
2025-10-21 23:03:45 +0200

    trew

--- public/registrer_student.php
+++ public/registrer_student.php
@@ -1,7 +1,22 @@
 <?php  
     include ("db.php"); /*kobler opp mot databasen*/
-    /*sjekker om vi har sendt inn ett skjema*/
+   if (isfet($_POST["submit"])) { /* Denne koden sjekker om vi har sendt inn et skjema */ /**/ 
+    $brukernavn=$_POST["brukernavn"]; /* det bruker skriver blir lagret her*/
+    $fornavn = $_POST["fornavn"];
+    $etternavn = $_POST["etternavn"];
+    $klassekode = $_POST["klassekode"];
 
+    if(!$brukernavn || !$fornavn || !$etternavn || !$klassekode) {/*sjekke at hvert felt er fyllt ut - || betyr eller . - !betyr motsatt / ikke fyllt ut noe*/
+        echo"<p style='color:red;'>Alle feltene må fylles ut.</p>";
+    } else{
+        $sqlSetning = "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode)
+        VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode')"; /*insert into skriver man hvilken kolonne man skal skrive i, value er verdien som blir satt i kolonna ørn*/
+
+       mysqli_query($db, $sqlSetning) or die("Ikke mulig å registrere student i databasen.");
+
+        echo "<p style='color:green;'>studenten <strong>$fornavn $etternavn</strong> ble registrert!</p>";
+    } 
+}
 
 ?>
 
@@ -12,8 +27,6 @@
 </head>
 <body>
   <h3>Register ny student</h3>
-
-  
     <form method="post" actio=""> <!-- lager et skjema boi-->
         <label>Brukenavn:</label><br>
         <input type="text" name="brukernavn"><br><br>
