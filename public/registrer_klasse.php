7c92fd09c11516628b9764d513390be9ec89abe2
Bjarnebrur <cadok9842@usn.no>
2025-10-22 19:11:22 +0200

    fgdas

--- public/registrer_klasse.php
+++ public/registrer_klasse.php
@@ -3,17 +3,31 @@ redigere registrer studie til å passe til klasse
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Eksempel 2</title>
+    <title>Registrer klasse</title>
 </head>
 <body>
-	<h3>Eksempel 2 </h3>
-    
-    <form method="post" action="tema04/eksempler/eksempel-2.php" id="eksempel2" name="eksempel2">
-        Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br />
-        Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br />
-		<input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
-		<input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
-	</form>
+    <h3>Registrer klasse</h3>
+
+    <form method="post" action="" id="registrer_klasse" name="registrer_klasse"> <!--skjema som samler inn data, id for js, name for php -->
+    Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br /> <!-- type - vanlig tekst felt, required gjør at feltet må fylles inn -->
+    Klassenavn <input type="klassenavn" id="klassenavn" name="klassenavn" required /> <br />
+    Studiumkode <input type="studiumkode" id="studiumkode" name="studiumkode" required /> <br />
+    <input type="submit" value="Fortsett" id="fortsett" name="fortsett" /> <!--lager en knapp -->
+    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
+
+    </form>
 </body>
 </html>
+
+<?php 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$klassekode = $_POST['klassekode'];
+$klassenavn = $_POST['klassenavn'];
+$studiumkode = $_POST['studiumkode'];
+
+echo "<p>Du har registrert klasse <strong>$klassenavn</strong> (klassekode) for studiumkode <strong>$studiumkode<strong>.</p>";
+
+}
+?>
 
