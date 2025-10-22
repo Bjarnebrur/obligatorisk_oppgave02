fb9589d928037e96b9f50ddca3bb10826adc6a6a
Bjarnebrur <cadok9842@usn.no>
2025-10-22 18:32:55 +0200

    fedastest

--- public/registrer_klasse.php
+++ public/registrer_klasse.php
@@ -1,1 +1,35 @@
 redigere registrer studie til å passe til klasse 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eksempel 2</title>
+</head>
+<body>
+	<h3>Eksempel 2 </h3>
+    
+    <form method="post" action="tema04/eksempler/eksempel-2.php" id="eksempel2" name="eksempel2">
+        Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br />
+        Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br />
+		<input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
+		<input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
+	</form>
+</body>
+</html>
+
+<?php  /*  Eksempel 2 */
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet lager et fullt navn ved å inkludere en egendefinert fuksjon 
+*/
+
+  include("funksjoner.php");  /* funksjoner inkludert */
+
+  $fornavn=$_POST ["fornavn"];
+  $etternavn=$_POST ["etternavn"];  
+
+  $navn=fulltNavn($fornavn,$etternavn);
+
+  print ("Navnet er $navn");  
+
+?>
