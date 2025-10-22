61a62217af8b9e71f804a02b0ff2ba5faf8e133d
Bjarnebrur <cadok9842@usn.no>
2025-10-22 18:38:40 +0200

    fgdsa

--- public/registrer_klasse.php
+++ public/registrer_klasse.php
@@ -17,19 +17,3 @@ redigere registrer studie til å passe til klasse
 </body>
 </html>
 
-<?php  /*  Eksempel 2 */
-/*
-/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
-/*    Programmet lager et fullt navn ved å inkludere en egendefinert fuksjon 
-*/
-
-  include("funksjoner.php");  /* funksjoner inkludert */
-
-  $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];  
-
-  $navn=fulltNavn($fornavn,$etternavn);
-
-  print ("Navnet er $navn");  
-
-?>
