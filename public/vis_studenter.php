ac674524f3db821f651c25815baf7c81f774f40d
Bjarnebrur <cadok9842@usn.no>
2025-10-23 20:55:26 +0200

    gfsd

--- public/vis_studenter.php
+++ public/vis_studenter.php
@@ -1,52 +1,38 @@
-<?php  /* vis-alle-poststeder / 
-/ Fra team 5 eks 1
-/*  Programmet skriver ut alle registrerte poststeder
-/
-  include("db.php");  / tilkobling til database-serveren utf�rt og valg av database foretatt /
-
-  $sqlSetning="SELECT FROM student;"; /* velger databasen i mysql /
-
-  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-    / SQL-setning sendt til database-serveren /
-
-  $antallRader=mysqli_num_rows($sqlResultat);  / antall rader i resultatet beregnet */
-
-  /*SELECT * FROM */
-?>
-
 <!DOCTYPE html>
-<html lang="no">
-  <head>
-    <meta charset="UTF-8">
-    <title>Registrerte studenter</title>
+<html>
+<head>
+  <title>Registrerte studenter</title>
 </head>
 <body>
-  <h3>Registerte studenter</h3>
 
-  <table border="1" cellpadding="5">
-    <tr>
-      <th>Brukernavn</th>
-      <th>Fornavn</th>
-      <th>Etternavn</th>
-      <th>Klassekode</th>
-  </tr>
+<?php  
 
-    <?php
-    for ($rad = 1; $rad <= $antallRader; $r++) {
-      $nåværendeRad = mysqli_fetch_array($sqlResultatet);
-      $brukernavn = $rad["brukernavn"];
-      $fornavn = $rad["fornavn"];
-      $etternavn = $rad["etternavn"];
-      $klassekode = $rad["klassekode"];
+  include("db.php"); 
 
-      echo "<tr>
-              <td>$brukeravn</td>
-              <td>$fornavn</td>
-              <td>$etternavn</td>
-              <td>$klassekode</td>
-            </tr>";
+  $sqlSetning="SELECT * FROM student;";
+  
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+  
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  
+  print ("<h3>Registrerte studenter</h3>");
+  print ("<table border=1>");  
+  print ("<tr><th align=left>brukernavn</th> <th align=left>fornavn</th> <th align=left>etternavn</th> <th align=left>klassekode</th></tr>"); /*tr= table row, th=table height */
+ 
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);
+      $brukernavn=$rad["brukernavn"];       
+      $fornavn=$rad["fornavn"];    
+      $etternavn=$rad["etternavn"]; 
+      $klassekode=$_post["klassekode"];
+
+      print ("<tr> <td> $brukernavn </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>"); /*forløkke, ittererererer igjennom hver rad og printer ut til slutt */
     }
-    ?>
-  </table>
+  print ("</table>"); 
+?>
+
+<p><a href="index.php">Tilbake til hovedmeny</a></p>
+
 </body>
 </html>
